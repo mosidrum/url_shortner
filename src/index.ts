1a33e44d1efd4e3148c293c5946aeb6519340dea
mosidrum <mosiokanga@gmail.com>
2024-05-23 16:47:43 +0100

    feat: server entry point

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Welcome to url shorter api');
+});
+
+app.listen(port, () => {
+  console.log(`Server started and listening on this port: ${port}`);
+});
