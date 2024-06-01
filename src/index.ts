1565b04a75ef6ae3a770b90a0d409a8aa3a33d81
mosidrum <mosiokanga@gmail.com>
2024-06-01 16:52:54 +0100

    add prettier and husky

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,14 @@ dotenv.config();
 const port = process.env.PORT || 8000;
 
 const serverPoint = 'unused';
-console.log("this is an eslint warning");
+console.log('this is an eslint warning');
 
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send('Welcome to url shorter api');
+  res.send('Welcome to url shorter api');
 });
 
 app.listen(port, () => {
-	console.log(`Server started and listening on port: ${port}`);
+  console.log(`Server started and listening on port: ${port}`);
 });
