2ca3f235c1b8e4338e3e0fbdae9ae20168dc4359
mosidrum <mosiokanga@gmail.com>
2024-06-01 15:39:13 +0100

    add basic server setup

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('Welcome to url shorter api');
+});
+
+app.listen(port, () => {
+	console.log(`Server started and listening on port: ${port}`);
+});
