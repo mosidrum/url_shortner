196db63a83a4ecd1d8eeba86386ecbbc8be1cf64
mosidrum <mosiokanga@gmail.com>
2024-06-07 07:58:56 +0100

    feat: add winston logger

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { logger } from "./services";
+
 dotenv.config();
 
 const port = process.env.PORT || 8000;
@@ -7,9 +9,16 @@ const port = process.env.PORT || 8000;
 const app = express();
 
 app.get('/', (req, res) => {
+	logger.info('GET / request was received successfully')
 	res.send('Welcome to url shorter api');
 });
 
 app.listen(port, () => {
+	logger.info(`Server started and listening on port: ${port}`)
 	console.log(`Server started and listening on port: ${port}`);
 });
+
+app.use((err: any, res: any) => {
+	logger.error(err.stack);
+	res.status(500).send('Something went wrong!')
+})
